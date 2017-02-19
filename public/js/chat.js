0d2440b8117543a80967dca1a21d52c3469afa8f
AndrewYanuta <andrew.yanuta@gmail.com>
2017-02-19 15:20:13 +0100

    add a join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,16 @@
 var socket = io()
 
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) messages.scrollTop(scrollHeight);
+}
+
 socket.on('connect', function () {
   console.log('Connected to server')
 })
@@ -16,7 +27,8 @@ socket.on('newMessage', function (message) {
     from: message.from,
     createAt: formatedTime
   });
-  jQuery('#messages').append(html)
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -27,7 +39,8 @@ socket.on('newLocationMessage', function (message) {
     from: message.from,
     createAt: formatedTime
   });
-  jQuery('#messages').append(html)
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
