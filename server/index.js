f15b1db2514b1af4216b5672abaa0f426f48a255
Andrew Yanuta <andrew.yanuta@gmail.com>
2020-01-28 22:24:52 +0200

    indentation

--- server/index.js
+++ server/index.js
@@ -67,13 +67,13 @@ io.on('connection', socket => {
   })
 
   socket.on('disconnect', () => {
-   var user = users.removeUser(socket.id)
+    var user = users.removeUser(socket.id)
 
-   if (user) {
-     io.to(user.room).emit('updateUserList', users.getUserList(user.room))
-     io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`))
-   }
- })
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`))
+    }
+  })
 })
 
 app.use(express.static(publicPath))
